3f40ebc7d158445051d3bdae16cc6f3bd0b41587
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-07-29 01:32:49 +0300

    добавит в main.js отрисовку компонентов

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
+import NewBriefTripInfo from './view/brief-trip-info.js';
 import NewFormListFilters from './view/list-filter.js';
-import LoaderTemplate from './view/loading-view.js';
-import {render} from './render.js';
+import NewTripSort from './view/trip-sort.js';
+import {render, RenderPosition} from './render.js';
+import BoardPresenter from './presenter/board-presenter.js';
 
+const tripMainElement = document.querySelector('.trip-main');
 const filtersElement = document.querySelector('.trip-controls__filters');
 const siteMainElement = document.querySelector('.page-main');
 const tripEventsElement = siteMainElement.querySelector('.trip-events');
+const boardPresenter = new BoardPresenter({boardContainer:tripEventsElement});
 
+render(new NewBriefTripInfo(), tripMainElement, RenderPosition.AFTERBEGIN);
 render(new NewFormListFilters(), filtersElement);
-render(new LoaderTemplate(), tripEventsElement);
+render(new NewTripSort(), tripEventsElement);
+
+boardPresenter.init();
