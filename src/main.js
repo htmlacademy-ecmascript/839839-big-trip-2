896736de65d435831d4b6505570010553fb1d93f
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-07-28 17:05:12 +0300

    добавит модуль list-filter.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+import NewFormListFilters from './view/list-filter.js';
 import LoaderTemplate from './view/loading-view.js';
 import {render} from './render.js';
 
+const filtersElement = document.querySelector('.trip-controls__filters');
 const siteMainElement = document.querySelector('.page-main');
 const tripEventsElement = siteMainElement.querySelector('.trip-events');
 
+render(new NewFormListFilters(), filtersElement);
 render(new LoaderTemplate(), tripEventsElement);
