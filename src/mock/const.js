7ff41f9438ff20b6bac8879651dcb6e18d3469b3
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-09-03 19:11:20 +0300

    изменяет регистр букв в константах

--- src/mock/const.js
+++ src/mock/const.js
@@ -1,4 +1,4 @@
-const TYPE = ['Taxi', 'Bus', 'Train', 'Ship', 'Drive', 'Flight', 'Check-in', 'Sightseeing', 'Restaurant'];
+const TYPE = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
 const FAVORITE = [true, false];
 
 export { TYPE, FAVORITE };
