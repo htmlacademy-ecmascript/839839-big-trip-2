d780b169cc450775db8156df2d6c235b8126daa0
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-09-03 19:28:33 +0300

    добавляет список дополнительных предложений

---
+++ src/mock/offers.js
@@ -0,0 +1,164 @@
+const offers = [
+  {
+    type: 'taxi',
+    offers: [
+      {
+        id: 1,
+        title: 'additional offer 1',
+        price: 120
+      },
+      {
+        id: 2,
+        title: 'additional offer 2',
+        price: 125
+      },
+      {
+        id: 3,
+        title: 'additional offer 3',
+        price: 50
+      }
+    ]
+  },
+  {
+    type: 'bus',
+    offers: [
+      {
+        id: 1,
+        title: 'additional offer 1',
+        price: 10
+      },
+      {
+        id: 2,
+        title: 'additional offer 2',
+        price: 125
+      },
+      {
+        id: 3,
+        title: 'additional offer 3',
+        price: 50
+      },
+      {
+        id: 4,
+        title: 'additional offer 4',
+        price: 75
+      }
+    ]
+  },
+  {
+    type: 'train',
+    offers: [
+      {
+        id: 1,
+        title: 'additional offer 1',
+        price: 65
+      },
+      {
+        id: 2,
+        title: 'additional offer 2',
+        price: 13
+      }
+    ]
+  },
+  {
+    type: 'ship',
+    offers: [
+      {
+        id: 1,
+        title: 'additional offer 1',
+        price: 95
+      },
+      {
+        id: 2,
+        title: 'additional offer 2',
+        price: 45
+      },
+      {
+        id: 3,
+        title: 'additional offer 3',
+        price: 50
+      },
+      {
+        id: 4,
+        title: 'additional offer 4',
+        price: 120
+      }
+    ]
+  },
+  {
+    type: 'drive',
+    offers: [
+      {
+        id: 1,
+        title: 'additional offer 1',
+        price: 120
+      },
+      {
+        id: 2,
+        title: 'additional offer 2',
+        price: 29
+      }
+    ]
+  },
+  {
+    type: 'flight',
+    offers: [
+      {
+        id: 1,
+        title: 'additional offer 1',
+        price: 74
+      },
+      {
+        id: 2,
+        title: 'additional offer 2',
+        price: 63
+      }
+    ]
+  },
+  {
+    type: 'check-in',
+    offers: [
+      {
+        id: 1,
+        title: 'additional offer 1',
+        price: 120
+      }
+    ]
+  },
+  {
+    type: 'sightseeing',
+    offers: [
+      {
+        id: 1,
+        title: 'additional offer 1',
+        price: 54
+      },
+      {
+        id: 2,
+        title: 'additional offer 2',
+        price: 85
+      },
+      {
+        id: 3,
+        title: 'additional offer 3',
+        price: 50
+      },
+      {
+        id: 4,
+        title: 'additional offer 4',
+        price: 120
+      }
+    ]
+  },
+  {
+    type: 'restaurant',
+    offers: [
+      {
+        id: 1,
+        title: 'additional offer 1',
+        price: 0
+      }
+    ]
+  }
+];
+
+export { offers };
