e5952aa8e3ae652a1ff1d8231a09ebfa3583b630
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-09-03 19:27:22 +0300

    добавляет список пунктов назначения

---
+++ src/mock/description.js
@@ -0,0 +1,69 @@
+export const destination = [
+  {
+    id: '1',
+    name: 'Chamonix',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    pictures: [
+      {
+        src: '#',
+        description: 'Chamonix parliament building',
+      }
+    ]
+  },
+  {
+    id: '2',
+    name: 'Barcelona',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    pictures: [
+      {
+        src: '#',
+        description: 'Barcelona parliament building',
+      }
+    ]
+  },
+  {
+    id: '3',
+    name: 'Berlin',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    pictures: [
+      {
+        src: '#',
+        description: 'Berlin parliament building',
+      }
+    ]
+  },
+  {
+    id: '4',
+    name: 'London',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    pictures: [
+      {
+        src: '#',
+        description: 'London parliament building',
+      }
+    ]
+  },
+  {
+    id: '5',
+    name: 'Dubai',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    pictures: [
+      {
+        src: '#',
+        description: 'Dubai parliament building',
+      }
+    ]
+  },
+  {
+    id: '6',
+    name: 'Moscow',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    pictures: [
+      {
+        src: '#',
+        description: 'Moscow parliament building',
+      }
+    ]
+  },
+];
+
