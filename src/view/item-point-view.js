12bc4896a80757ae295ef6ced6541f5079a06611
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-09-17 03:16:07 +0300

    добавляет получение данных компонентом и передает их в шаблон

--- src/view/item-point-view.js
+++ src/view/item-point-view.js
@@ -1,23 +1,30 @@
 import { createElement } from '../render.js';
+import { formatDate } from '../utils.js';
+import { DATE_FORMAT, DATE_FORMAT_MAX, TIME_FORMAT, DATA_TIME_FORMAT } from '../const.js';
 
-const createItemPoint = () =>
-  `<li class="trip-events__item">
+const createItemPoint = (point) => {
+  const {type, basePrice, dateFrom, dateTo, isFavorite} = point;
+
+  const favoriteClassName = isFavorite ? 'event__favorite-btn--active' : '';
+
+  return (
+    `<li class="trip-events__item">
       <div class="event">
-        <time class="event__date" datetime="2019-03-18">MAR 18</time>
+        <time class="event__date" datetime="${formatDate(dateFrom, DATE_FORMAT_MAX)}">${formatDate(dateFrom, DATE_FORMAT)}</time>
         <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
         </div>
-        <h3 class="event__title">Taxi Amsterdam</h3>
+        <h3 class="event__title">${type} Amsterdam</h3>
         <div class="event__schedule">
           <p class="event__time">
-            <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+            <time class="event__start-time" datetime="${formatDate(dateFrom, DATA_TIME_FORMAT)}">${formatDate(dateFrom, TIME_FORMAT)}</time>
             &mdash;
-            <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+            <time class="event__end-time" datetime="${formatDate(dateTo, DATA_TIME_FORMAT)}">${formatDate(dateTo, TIME_FORMAT)}</time>
           </p>
           <p class="event__duration">30M</p>
         </div>
         <p class="event__price">
-          &euro;&nbsp;<span class="event__price-value">20</span>
+          &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
         </p>
         <h4 class="visually-hidden">Offers:</h4>
         <ul class="event__selected-offers">
@@ -27,7 +34,7 @@ const createItemPoint = () =>
             <span class="event__offer-price">20</span>
           </li>
         </ul>
-        <button class="event__favorite-btn event__favorite-btn--active" type="button">
+        <button class="event__favorite-btn ${favoriteClassName}" type="button">
           <span class="visually-hidden">Add to favorite</span>
           <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
             <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
@@ -37,11 +44,16 @@ const createItemPoint = () =>
           <span class="visually-hidden">Open event</span>
         </button>
       </div>
-    </li>`;
+    </li>`);
+};
 
 export default class ItemPointView {
+  constructor({point}) {
+    this.point = point;
+  }
+
   getTemplate() {
-    return createItemPoint();
+    return createItemPoint(this.point);
   }
 
   getElement() {
