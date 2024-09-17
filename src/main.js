ec4cffa1e90c7bddb82222df5d63918788b421f5
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-09-17 03:18:16 +0300

    Передает mocks данные в presenter

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import FiltersView from './view/filters-view.js';
 import SortView from './view/sort-view.js';
 import { render, RenderPosition } from './render.js';
 import TripPresenter from './presenter/trip-presenter.js';
+import PointModel from './model/point-model.js';
 
 const tripMainElement = document.querySelector('.trip-main');
 const filtersElement = document.querySelector('.trip-controls__filters');
 const siteMainElement = document.querySelector('.page-main');
 const tripEventsElement = siteMainElement.querySelector('.trip-events');
-const tripPresenter = new TripPresenter({ boardContainer: tripEventsElement });
+
+const pointModel = new PointModel();
+const tripPresenter = new TripPresenter({
+  boardContainer: tripEventsElement,
+  pointModel
+});
 
 render(new RouteView(), tripMainElement, RenderPosition.AFTERBEGIN);
 render(new FiltersView(), filtersElement);
