8434686439c27b14817a5da4dc6032f772809f7c
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-11-15 17:06:15 +0300

    удаляет типы фильтров

--- src/mock/const.js
+++ src/mock/const.js
@@ -1,10 +1,3 @@
 const FAVORITE = [true, false];
 
-const FilterType = {
-  EVERYTHING: 'everything',
-  FUTURE: 'future',
-  PRESENT: 'present',
-  PAST: 'past',
-};
-
-export { FAVORITE, FilterType };
+export { FAVORITE };
