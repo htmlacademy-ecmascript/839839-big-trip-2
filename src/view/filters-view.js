919cb74290d89a286bdd255e6d2feffa1c72fc62
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-11-16 20:14:43 +0300

    вносит правки

--- src/view/filters-view.js
+++ src/view/filters-view.js
@@ -3,21 +3,21 @@ import AbstractView from '../framework/view/abstract-view.js';
 /**
  * Создает шаблон элемента фильтра.
  *
- * @param {Object} filter - Объект фильтра.
+ * @param {Object} filter - Объект фильтров.
  * @param {boolean} isChecked - Флаг, указывающий, на выбранный фильтр.
  * @returns {string} элемент фильтра.
  */
 const createFilterItemTemplate = (filter, isChecked) => {
   const {type, count} = filter;
 
-  return `<div class="trip-filters__filter">
+  return (`<div class="trip-filters__filter">
       <input id="filter-${type}"
       class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter"
       value="${type}"
       ${isChecked ? 'checked' : ''}
       ${count === 0 ? 'disabled' : ''}>
       <label class="trip-filters__filter-label" for="filter-future">${type}</label>
-    </div>`;
+    </div>`);
 };
 
 /**
@@ -29,7 +29,7 @@ const createFilterItemTemplate = (filter, isChecked) => {
 const createFilters = (filters) =>
   `<form class="trip-filters" action="#" method="get">
 
-    ${filters.map((filter, index) => createFilterItemTemplate(filter, index === 0)).join('')}
+    ${filters.map((filter, index) => createFilterItemTemplate(filter, index)).join('')}
 
     <button class="visually-hidden" type="submit">Accept filter</button>
   </form>`;
