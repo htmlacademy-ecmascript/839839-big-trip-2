1e4a70f952463d84589a4b50126a3054b835d7a3
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-11-18 22:39:27 +0300

    добавляет функцию обновления точки маршрута в списке путешествий

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -22,4 +22,14 @@ const getTimeDifference = (dateStart, dateEnd) => {
   }
 };
 
-export { formatDate, getTimeDifference };
+/**
+ * Обновляет точку путешествия в списке.
+ *
+ * @param {Array<Object>} items - Массив объектов, который необходимо обновить.
+ * @param {Object} update - Объект, содержащий новые данные.
+ * @returns {Array<Object>}  Новый массив с обновленным элементом.
+ */
+const updateItem = (items, update) =>
+  items.map((item) => item.id === update.id ? update : item);
+
+export { formatDate, getTimeDifference, updateItem };
