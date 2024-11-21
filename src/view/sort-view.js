8fe89ed6cc8d40182526c2071a631d6ce6a258cf
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-11-21 23:30:28 +0300

    добавляет: обработчик событий в sort-view и атрибут dataset

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,18 +1,35 @@
 import AbstractView from '../framework/view/abstract-view.js';
 import { SortType } from '../const.js';
 
-const createSort = () =>
+const createSortTemplate = () =>
   (`<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
 
     ${Object.values(SortType).map((sort) => `<div class="trip-sort__item  trip-sort__item--${sort}">
-      <input id="sort-${sort}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${sort}" checked>
+      <input id="sort-${sort}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${sort}" data-sort-type="${sort}" checked>
       <label class="trip-sort__btn" for="sort-${sort}">${sort}</label>
     </div>`).join('')}
 
   </form>`);
 
 export default class SortView extends AbstractView {
-  get template() {
-    return createSort();
+  #handleSortTypeChange = null;
+
+  constructor({onSortTypeChange}) {
+    super();
+
+    this.#handleSortTypeChange = onSortTypeChange;
+    this.element.addEventListener('click', this.#onSortFormClick);
   }
+
+  get template() {
+    return createSortTemplate();
+  }
+
+  #onSortFormClick = (evt) => {
+    if(evt.target.tagName !== 'INPUT') {
+      return;
+    }
+    evt.preventDefault();
+    this.#handleSortTypeChange(evt.target.dataset.sortType);
+  };
 }
