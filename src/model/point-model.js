79d2ad57312d268ce429d8dae6be13e38f7059bd
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-12-06 18:05:03 +0300

    добавит для pointModel наследование от класса Observable

--- src/model/point-model.js
+++ src/model/point-model.js
@@ -1,15 +1,16 @@
+import Observable from '../framework/observable.js';
 import { getRandomPoint } from '../mock/data.js';
 import { mockDestinations } from '../mock/description.js';
 import { mockOffers } from '../mock/offers.js';
 
 const POINT_COUNT = 4;
 
-export default class PointModel {
+export default class PointModel extends Observable {
   #points = Array.from({length: POINT_COUNT}, getRandomPoint);
   #allDestinations = mockDestinations;
   #allOffers = mockOffers;
 
-  get point() {
+  get points() {
     return this.#points;
   }
 
