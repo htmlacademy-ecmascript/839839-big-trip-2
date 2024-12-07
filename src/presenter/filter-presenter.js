6810c40699b5a04b92299a3626439ab6cfe5d9d3
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-12-07 23:33:22 +0300

    добавляет filter presenter

---
+++ src/presenter/filter-presenter.js
@@ -0,0 +1,57 @@
+import { FilterType, UpdateType } from '../const';
+import { remove, render, replace } from '../framework/render.js';
+import FiltersView from '../view/filters-view.js';
+import { filter } from '../utils/filter.js';
+
+export default class FilterPresenter {
+  #filterContainer = null;
+  #filterModel = null;
+  #pointModel = null;
+
+  #filterComponent = null;
+
+  constructor({filterContainer, filterModel, pointModel}) {
+    this.#filterContainer = filterContainer;
+    this.#filterModel = filterModel;
+    this.#pointModel = pointModel;
+
+    // this.#pointModel.addObserver(this.#handleModelEvent);
+    // this.#filterModel.addObserver(this.#handleModelEvent);
+  }
+
+  get filters() {
+    const points = this.#pointModel.points;
+
+    return Object.values(FilterType).map((type) => ({
+      type,
+      count: filter[type](points).length
+    }));
+  }
+
+  init() {
+    const filters = this.filters;
+    const prevFilterComponent = this.#filterComponent;
+
+    this.#filterComponent = new FiltersView({
+      filters,
+      currentFilterType: this.#filterModel.filter,
+      onFilterTypeChange: this.#handleFilterTypeChange,
+    });
+
+    if (prevFilterComponent === null) {
+      render(this.#filterComponent, this.#filterContainer);
+      return;
+    }
+
+    replace(this.#filterComponent, prevFilterComponent);
+    remove(prevFilterComponent);
+  }
+
+  #handleFilterTypeChange = (filterType) => {
+    if (this.#filterModel.filter === filterType) {
+      return;
+    }
+
+    this.#filterModel.setFilter(UpdateType.MAJOR, filterType);
+  };
+}
