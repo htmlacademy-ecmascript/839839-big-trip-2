390af3e855cd0694f086c7f88b5fcfc8b798a85f
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-12-07 23:59:53 +0300

    переименует файл кнопки New Event

--- src/view/button-new-event-view.js
+++ src/view/button-new-event-view.js
@@ -3,7 +3,7 @@ import AbstractView from '../framework/view/abstract-view.js';
 const createButtonNewEventTemplate = () =>
   ('<button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button">New event</button>');
 
-export default class ButtonNewEvent extends AbstractView {
+export default class ButtonNewEventView extends AbstractView {
   get template() {
     return createButtonNewEventTemplate();
   }
