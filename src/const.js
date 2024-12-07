881b7ab7fa01e57ec9e6c20b60369011b8b416a7
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-12-07 23:32:22 +0300

    добавляет список сообщений для фильтров

--- src/const.js
+++ src/const.js
@@ -20,7 +20,10 @@ const TimeFormat = {
 };
 
 const Message = {
-  NEW_EVENT: 'Click New Event to create your first point',
+  EVERYTHING: 'Click New Event to create your first point',
+  PAST: 'There are no past events now',
+  PRESENT: 'There are no present events now',
+  FUTURE: 'There are no future events now',
 };
 
 const SortType = {
